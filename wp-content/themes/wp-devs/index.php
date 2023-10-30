227b5d2b3a543a765278b50eef47e88d86e7306e
dragan002 <draganvujic29@gmail.com>
2023-10-30 12:18:22 +0100

    Creating and activatuib wp-theme

---
+++ wp-content/themes/wp-devs/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wp-devs-title</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus recusandae modi autem minus, iusto saepe quo necessitatibus omnis quibusdam corrupti.</p>
+</body>
+</html>
