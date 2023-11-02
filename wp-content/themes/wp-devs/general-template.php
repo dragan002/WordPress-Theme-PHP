f1e5221f9c13ad8b8dee0b47fa9a0b85d4ee615b
dragan002 <draganvujic29@gmail.com>
2023-11-02 16:51:53 +0100

    Added header

--- wp-content/themes/wp-devs/general-template.php
+++ wp-content/themes/wp-devs/general-template.php
@@ -4,6 +4,10 @@
 ?>
 
 <?php get_header(); ?>
+
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="">
+
+<?php add_theme_support('') ?>
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
--- wp-content/themes/wp-devs/page.php
+++ wp-content/themes/wp-devs/page.php
@@ -1,4 +1,7 @@
 <?php get_header(); ?>
+
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="">
+
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
