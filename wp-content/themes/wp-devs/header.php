37b7124c8cf78b8150a23bd68f280f04d73f8090
dragan002 <draganvujic29@gmail.com>
2023-11-02 14:46:18 +0100

    Implementing drop down menu and dynamics posts for blog page

--- wp-content/themes/wp-devs/header.php
+++ wp-content/themes/wp-devs/header.php
@@ -21,6 +21,13 @@
             <section class="menu-area">
                 <div class="container">
                     <nav class="main-menu">
+                        <button class="check-button">
+                            <div class="menu-icon">
+                                <div class="bar1"></div>
+                                <div class="bar2"></div>
+                                <div class="bar3"></div>
+                            </div>
+                        </button>
                        <?php wp_nav_menu(array('theme_location' => 'wp_devs_main_menu','depth' => 2)); ?>
                     </nav>
                 </div>
--- wp-content/themes/wp-devs/js/dropdown.js
+++ wp-content/themes/wp-devs/js/dropdown.js
@@ -1,1 +1,14 @@
-console.log('test');
+let dropdown = document.querySelector('.menu');
+submenu = document.querySelector('.sub-menu');
+buttonClick = document.querySelector('.check-button');
+hamburger = document.querySelector('.menu-icon');
+
+buttonClick.addEventListener('click', () => {
+    dropdown.classList.toggle('show-dropdown');
+    if ( submenu ) {
+        submenu.classList.toggle('show-dropdown');
+    }
+   
+    hamburger.classList.toggle('animate-button');
+    
+})
