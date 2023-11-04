b6bb28eb902223551da78fb200b33806558b69d7
dragan002 <draganvujic29@gmail.com>
2023-11-04 09:42:56 +0100

    Permalink added

--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -12,14 +12,14 @@
                         while (have_posts()) : the_post();
                     ?>
                     <article>
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_post_thumbnail(array(275, 275)); ?>
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <a href="<?php the_permalink(); ?>"> <?php the_post_thumbnail(array(275, 275)); ?></a>       
                         <div class="meta-info">
                             <p>Posted in <?php echo get_the_date(); ?> by the <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></p>
                             <p>Categories: <?php the_category(' ') ?></p>
                             <p>Tags: <?php the_tags('', ', ') ?></p>
                         </div>
-                        <?php the_content(); ?>
+                        <?php the_excerpt(); ?>
                     </article>
                     <?php
                         endwhile;
--- wp-content/themes/wp-devs/page-home.php
+++ wp-content/themes/wp-devs/page-home.php
@@ -46,8 +46,8 @@
                         while ($postlist->have_posts()) : $postlist->the_post();
                     ?>
                     <article class="latest-news">
-                        <?php the_post_thumbnail('large'); ?>
-                        <h3><?php the_title(); ?></h3>
+                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a>
+                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                         <div class="meta-info">
                             <p>
                                 Posted in <?php echo get_the_date(); ?> by the <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a>
