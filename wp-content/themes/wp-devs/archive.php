52796b8e0fa0a44c1e4be99e5da2f0c6fbbffc65
dragan002 <draganvujic29@gmail.com>
2023-11-04 17:27:07 +0100

    Archive

--- wp-content/themes/wp-devs/archive.php
+++ wp-content/themes/wp-devs/archive.php
@@ -4,9 +4,10 @@
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
-            <h1>Blog</h1>
+            <?php the_archive_title('<h1 class="archive-title>">', '</h1>') ?>
+            <?php the_archive_description('<div class="archive-description>">', '</div>') ?>
             <div class="container">
-                <div class="blog-items">
+                <div class="archive-items">
                     <?php  
                     if (have_posts()):
                         while (have_posts()) : the_post();
